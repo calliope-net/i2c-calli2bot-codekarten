d20d8be556b2c2d717c78d6244b23971a73c2eb1
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-11-21 19:49:34 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+function Seite5fahren50cm () {
+    Calli2bot.setMotoren2(100, 100)
+    basic.pause(3200)
+    Calli2bot.setMotoren2(0, 0)
+}
+function Seite5drehen () {
+    Calli2bot.setMotoren2(100, -100)
+    basic.pause(1370)
+    Calli2bot.setMotoren2(0, 0)
+}
 function Seite3LED () {
     Calli2bot.setRgbLed3(0xff0000, true, false, false, false, false)
     basic.pause(100)
@@ -24,6 +34,12 @@ function Seite4StopandGo () {
         Calli2bot.setMotoren2(0, 0)
     }
 }
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    for (let index = 0; index < 4; index++) {
+        Seite5fahren50cm()
+        Seite5drehen()
+    }
+})
 function Seite2Motor () {
     Calli2bot.setMotoren2(100, 100)
     calli2bot.pause(calli2bot.calli2bot_ePause(calli2bot.ePause.p1))
@@ -46,5 +62,5 @@ let Calli2bot: calli2bot.Calli2bot = null
 Calli2bot = calli2bot.beimStart(calli2bot.calli2bot_eADDR(calli2bot.eADDR.CB2_x22))
 lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
 basic.forever(function () {
-    Seite4StopandGo()
+	
 })
